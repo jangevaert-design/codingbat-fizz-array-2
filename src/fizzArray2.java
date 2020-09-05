30bb1d1673487d055bf3025613a1c44b633af700
Jan Gevaert <jan.gevaert@gmail.com>
2020-09-05 06:32:48 -0600

    Initial commit

---
+++ src/fizzArray2.java
@@ -0,0 +1,10 @@
+public class fizzArray2 {
+  public String[] fizzArray2(int n) {
+    String[] fizz = new String[n];
+
+    for (int i =0; i < n; i++) {
+      fizz[i] = String.valueOf(i);
+    }
+    return fizz;
+  }
+}
